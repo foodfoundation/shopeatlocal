72dfdefca9f6bea0f8980ee27b9df75249276725
Sumit <techiesumit@gmail.com>
2025-05-02 21:15:44 -0500

    Draft Commit
    Draft Commit: Code not Working

--- producers-report/src/report-filter.js
+++ producers-report/src/report-filter.js
@@ -1,59 +1,5 @@
-// @ts-nocheck
-import FilterableTable from "react-filterable-table";
-import "./App.css";
-import { BsSortUp, BsSortDown } from "react-icons/bs";
-import { json2csv } from "json-2-csv";
-//import { useRef } from "react";
-//import { fields } from "./test_data";
-
-function App() {
-  const createCsv = () => {
-    try {
-      // @ts-ignore
-      const filterValue= document.querySelector(".filter-container > input")?.value;
-      console.log("filterValue", filterValue);
-      const filterData = ReportFilterAndSort(window.ProducerData.data, {filter:filterValue, fields:window.ProducerData.fields});
-      console.log("filterData", filterData);
-      const csv = json2csv(window.ProducerData.data);
-
-      const blob = new Blob([csv], { type: "text/csv" });
-      const url = window.URL.createObjectURL(blob);
-      const a = document.createElement("a");
-      a.setAttribute("href", url);
-      a.setAttribute("download", "producer-sales-report.csv");
-      a.click();
-    } catch (error) {
-      alert("We encountered an error generating your CSV. Please try again later.");
-      console.error("generating_csv_error:", error);
-    }
-  };
-
-  return (
-    <div className="App">
-      <div className="d-flex flex-row justify-content-between p-2">
-        <button type="button" className="btn btn-outline-primary" onClick={createCsv}>
-          Export CSV
-        </button>
-      </div>
-      <div className="reports">
-        <FilterableTable
-          namespace="Producer Reports"
-          initialSort="Cyc.IDCyc"
-          data={window.ProducerData.data}
-          fields={window.ProducerData.fields}
-          noRecordsMessage="There is no data to display"
-          noFilteredRecordsMessage="No rows match your filters!"
-          iconSortedDesc={<BsSortUp />}
-          iconSortedAsc={<BsSortDown />}
-          pageSizes={[20, 50, 100, 1000]}
-        />
-      </div>
-    </div>
-  );
-}
-
-export default App;
-
+import hasValue from './hasValue';
+import getValue from './getValue';
 
 function ReportFilterAndSort(array, options) {
 	array = array || [];
@@ -209,13 +155,4 @@ function MultiSort(array, keys) {
 	return array;
 };
 
-function getValue(record, field) {
-	if (field.indexOf(".") > 0) {
-		return field.split('.').reduce((o,i)=>o ? o[i] : null, record);
-	}
-	return record[field];
-}
-
-function hasValue(val) {
-	return val !== null && val !== undefined && val.toString().length > 0;
-}
+export default ReportFilterAndSort;
