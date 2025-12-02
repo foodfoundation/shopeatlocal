4dd191f9723e765a6808076ae42f39578c58c99d
Peter Holczbauer <holczpeti@gmail.com>
2025-12-02 09:51:21 +0100

    setup i18n

---
+++ app/src/i18n.js
@@ -0,0 +1,27 @@
+// src/i18n.js
+import i18next from "i18next";
+import Backend from "i18next-fs-backend";
+import middleware from "i18next-http-middleware";
+import { join } from "path";
+
+i18next
+  .use(Backend)
+  .use(middleware.LanguageDetector)
+  .init({
+    fallbackLng: "en",
+    supportedLngs: ["en", "de"],
+    preload: ["en", "de"],
+    ns: ["common", "forms", "errors", "flash"],
+    defaultNS: "common",
+    backend: {
+      loadPath: join(process.cwd(), "locales/{{lng}}/{{ns}}.json"),
+    },
+    detection: {
+      order: ["cookie", "querystring", "header"],
+      caches: ["cookie"],
+      cookieName: "locale",
+      lookupQuerystring: "lang",
+    },
+  });
+
+export default i18next;
