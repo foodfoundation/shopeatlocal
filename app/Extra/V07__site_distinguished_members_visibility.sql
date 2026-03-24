ab5f114a7fb83b261d2e460481b9d98f36920ba9
Peter Holczbauer <holczpeti@gmail.com>
2026-03-24 15:13:34 +0100

    cleanup

--- app/Extra/V07__site_distinguished_members_visibility.sql
+++ app/Extra/V07__site_distinguished_members_visibility.sql
@@ -1,3 +1,2 @@
--- Active: 1773225539253@@127.0.0.1@3306@ifcom_wholesale
 ALTER TABLE `Site`
 ADD COLUMN `CkShowDistinguishedMembersPage` TINYINT(1) NOT NULL DEFAULT '1';
--- app/Extra/V08_site_cycle_length.sql
+++ app/Extra/V08_site_cycle_length.sql
@@ -1,3 +1,2 @@
--- Active: 1773225539253@@127.0.0.1@3306@ifcom_wholesale
 ALTER TABLE `Site`
 ADD COLUMN `QtyCycleLength` int unsigned NOT NULL DEFAULT '2';
